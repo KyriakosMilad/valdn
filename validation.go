194fdfc7311bd8784773dea3353100d5845feff4
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-05-16 07:43:51 +0200

    Validate map

--- validation.go
+++ validation.go
@@ -6,8 +6,7 @@ import (
 	"reflect"
 )
 
-func Validate(jsonData string, validationRules map[string][]string) (err error, validationErrors map[string]string) {
-	validationErrors = make(map[string]string)
+func ValidateJson(jsonData string, validationRules map[string][]string) (err error, validationErrors map[string]string) {
 	var decodedJson map[string]interface{}
 
 	err = json.Unmarshal([]byte(jsonData), &decodedJson)
@@ -15,8 +14,14 @@ func Validate(jsonData string, validationRules map[string][]string) (err error, 
 		return err, nil
 	}
 
+	return ValidateMap(decodedJson, validationRules)
+}
+
+func ValidateMap(mapData map[string]interface{}, validationRules map[string][]string) (err error, validationErrors map[string]string) {
+	validationErrors = make(map[string]string)
+
 	for key, rules := range validationRules {
-		val, ok := decodedJson[key]
+		val, ok := mapData[key]
 
 		for _, v := range rules {
 			switch v {
--- validation_test.go
+++ validation_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestValidate(t *testing.T) {
+func TestValidateJson(t *testing.T) {
 	type args struct {
 		jsonData        string
 		validationRules map[string][]string
@@ -36,7 +36,50 @@ func TestValidate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err, validationErrors := Validate(tt.args.jsonData, tt.args.validationRules)
+			err, validationErrors := ValidateJson(tt.args.jsonData, tt.args.validationRules)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Validate() error = %v, wantErr %v, args %v", err, tt.wantErr, tt.args)
+			}
+			if (len(validationErrors) > 0) != tt.wantValidationErrors {
+				t.Errorf("Validate() error = %v, wantValidationErrors %v, args %v", validationErrors, tt.wantErr, tt.args)
+			}
+		})
+	}
+}
+
+func TestValidateMap(t *testing.T) {
+	type args struct {
+		mapData         map[string]interface{}
+		validationRules map[string][]string
+	}
+	tests := []struct {
+		name                 string
+		args                 args
+		wantErr              bool
+		wantValidationErrors bool
+	}{
+		{
+			name: "test validate map",
+			args: args{
+				mapData:         map[string]interface{}{"name": "Ramses", "city": "Tiba"},
+				validationRules: map[string][]string{"name": {"required", "string"}, "city": {"required", "string"}},
+			},
+			wantErr:              false,
+			wantValidationErrors: false,
+		},
+		{
+			name: "test validate map with unsuitable data",
+			args: args{
+				mapData:         map[string]interface{}{"name": "Ramses", "age": 90},
+				validationRules: map[string][]string{"name": {"required", "string"}, "city": {"required", "string"}, "age": {"required", "numerical"}},
+			},
+			wantErr:              false,
+			wantValidationErrors: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err, validationErrors := ValidateMap(tt.args.mapData, tt.args.validationRules)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Validate() error = %v, wantErr %v, args %v", err, tt.wantErr, tt.args)
 			}
